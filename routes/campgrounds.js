f25db4833c108b88a38ade58ea8ad88075048fef
Patrick Brennan <padist3r@gmail.com>
2021-02-19 09:09:10 +0000

    started adding image uploading capabilities

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,12 +4,18 @@ const asyncCatch = require("../utilities/asyncCatch.js");
 const Campground = require("../models/campground.js");
 const { isLoggedIn, validateCampground, isAuthor } = require("../middleware.js");
 const campgrounds = require("../controllers/campgrounds.js")
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 
 // routes --------------------------------------------------------------------------
 router.route("/")
     .get(asyncCatch(campgrounds.index))
-    .post(isLoggedIn, validateCampground, asyncCatch(campgrounds.createCampground));
+    // .post(isLoggedIn, validateCampground, asyncCatch(campgrounds.createCampground));
+    .post(upload.array("image"), (req, res) => {
+        console.log(req.body, req.files)
+        res.send("it worked")
+    })
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
