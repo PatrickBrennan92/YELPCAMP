10bcdb03883b15b986174020c507e49c36ea8117
Patrick Brennan <padist3r@gmail.com>
2021-02-24 12:04:59 +0000

    added maps to show page and map now displays camp location on creation of a new camp

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,7 +1,18 @@
+const camp = JSON.parse(campground)
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [30, 30], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    center: camp.geometry.coordinates, // starting position [lng, lat]
+    zoom: 6 // starting zoom
 });
+
+new mapboxgl.Marker()
+    .setLngLat(camp.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+        .setHTML(
+            `<h5>${camp.title}</h5><p>${camp.description}</p>`
+        )
+    )
+    .addTo(map)
